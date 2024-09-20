e47be8715a180081b4860be491f1ad689e7be251
luciano lujan <luciano.i.lujan@gmail.com>
2024-09-20 13:52:24 -0300

    Se Modifica Readme con las concluciones la carpeta Configuración de módulos de comunicación

--- C prototipo/Sprint 2/Configuración de módulos de comunicación/Wifi/src/main.cpp
+++ C prototipo/Sprint 2/Configuración de módulos de comunicación/Wifi/src/main.cpp
@@ -1,51 +1,56 @@
 #include <Arduino.h>
-#include<ESP8266WiFiMulti.h>
-#include<ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-const char* ssid = "wifi";
-const char* password = "Contra";
+const char* ssid = "wifi";           // Nombre de la red Wi-Fi
+const char* password = "Contra";     // Contraseña de la red Wi-Fi
 
-String user = "ioticos";
-String pass = "pingpong";
+String user = "xxxx";             // Datos de usuario a enviar
+String pass = "xxxx";
 
 void setup() {
-  delay(10);
   Serial.begin(115200);
+  delay(10);
 
- WiFi.begin("conectando...");
- while (WiFi.status() != WL_CONNECTED)
- {
-  delay(500);
-  Serial.print(".");
- }
- Serial.print("contado con exito, mi ip es;");
- Serial.println(WiFi.localIP());
-
-
+  // Conectar a Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando a Wi-Fi");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.print("Conectado con éxito, IP asignada: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  if(WiFi.status() == WL_CONNECTED){
-    HTTPClient http;
-    String datos_a_enviar = "?user=" + user + "&pass" + pass;
+  if (WiFi.status() == WL_CONNECTED) {  // Verificar si está conectado a Wi-Fi
+    HTTPClient http;                    // Crear un objeto HTTPClient
+    String datos_a_enviar = "?user=" + user + "&pass=" + pass; // Datos a enviar
     
-    http.begin("http://wwww.php");
-    http.addHeader("conte.Type","aplication" );
-    int codigo_respuesta =http.POST(datos_a_enviar);
+    http.begin("http://www.tuservidor.com/php");  // URL del servidor
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");  // Añadir cabecera de contenido
+    int codigo_respuesta = http.POST(datos_a_enviar);  // Enviar datos vía POST
 
-    if(codigo_respuesta >=0){
-      Serial.println("codigo HTTP " + String(codigo_respuesta));
+    if (codigo_respuesta > 0) {
+      Serial.println("Código HTTP: " + String(codigo_respuesta));
       
-      if(codigo_respuesta == 200){
-        String cuerpo_respuessta = http.getString();
-        Serial.println("El servidor respondio");
-        Serial.println(cuerpo_respuessta);
+      if (codigo_respuesta == HTTP_CODE_OK) {  // Si el servidor respondió correctamente
+        String respuesta = http.getString();   // Leer respuesta del servidor
+        Serial.println("Respuesta del servidor:");
+        Serial.println(respuesta);
       }
-      http.end();
-    }else{
-      Serial.println("Error en la conexion wifi");
+    } else {
+      Serial.println("Error en la conexión HTTP.");
     }
-    delay(2000);
+
+    http.end();  // Cerrar la conexión
+  } else {
+    Serial.println("No conectado a Wi-Fi.");
   }
 
+  delay(2000);  // Esperar 2 segundos para la siguiente solicitud
 }
