b7532f1b99d15154930094060e2f288980f9db28
vittoriodurigutti <vittodutti@gmail.com>
2024-10-01 22:32:11 -0300

    actualizacion 1/10b

--- D presentacion/Sprint 2/Prototipo Integrado Final 1/src/main.cpp
+++ D presentacion/Sprint 2/Prototipo Integrado Final 1/src/main.cpp
@@ -2,6 +2,12 @@
 #include "conexiones/wifi.h"
 #include "conexiones/lora.h"
 #include "conexiones/ble.h"
+#include "sensores/sensor_aht10.h"
+#include "sensores/sensor_bh1750.h"
+#include "sensores/sensor_dht11.h"
+#include "sensores/sensor_ldr.h"
+#include "sensores/sensor_hcsr04.h"
+#include "sensores/sensor_humedad_suelo.h"
 
 void setup() {
   Serial.begin(115200);
@@ -9,12 +15,55 @@ void setup() {
   // Inicialización de conexiones
   iniciarWiFi();
   iniciarLoRa();
-  iniciarBle();
+  iniciarBluetooth();
+
+  // Inicialización de sensores
+  iniciarAHT10();
+  iniciarBH1750();
+  iniciarDHT11();
+  iniciarLDR();
+  iniciarUltrasonico();
+  iniciarHumedadSuelo();
 }
 
 void loop() {
-  // Puedes usar condicionales o lógicas para diferentes tareas aquí.
-  manejarWiFi();     // Manejamos la conexión y envío WiFi
-  manejarLoRa();     // Manejamos la recepción y envío de LoRa
-  manejarBle(); // Manejamos la comunicación vía Bluetooth
+  // Manejo de conexiones
+  manejarWiFi();
+  manejarLoRa();
+  manejarBluetooth();
+
+  // Lecturas de los sensores
+  float temperaturaAHT10 = leerTemperaturaAHT10();
+  float humedadAHT10 = leerHumedadAHT10();
+  Serial.print("AHT10 - Temp: ");
+  Serial.print(temperaturaAHT10);
+  Serial.print(" °C, Humedad: ");
+  Serial.println(humedadAHT10);
+
+  float lux = leerLuxBH1750();
+  Serial.print("BH1750 - Luz: ");
+  Serial.print(lux);
+  Serial.println(" lux");
+
+  float temperaturaDHT11 = leerTemperaturaDHT11();
+  float humedadDHT11 = leerHumedadDHT11();
+  Serial.print("DHT11 - Temp: ");
+  Serial.print(temperaturaDHT11);
+  Serial.print(" °C, Humedad: ");
+  Serial.println(humedadDHT11);
+
+  int nivelLuzLDR = leerNivelLuzLDR();
+  Serial.print("LDR - Nivel de luz: ");
+  Serial.println(nivelLuzLDR);
+
+  long distancia = leerDistanciaUltrasonico();
+  Serial.print("HC-SR04 - Distancia: ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+
+  int humedadSuelo = leerHumedadSuelo();
+  Serial.print("HL-69 - Humedad del suelo: ");
+  Serial.println(humedadSuelo);
+
+  delay(2000);  // Retraso entre lecturas
 }
